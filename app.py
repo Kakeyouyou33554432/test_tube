148fa00ff81ff09d1ccde3b9b30a093cd260f14c
Kakeyouyou33554432 <myyn32768@gmail.com>
2025-08-02 00:55:24 +0900

    Update app.py

--- app.py
+++ app.py
@@ -22,22 +22,31 @@ intents = discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents=intents)
 
-# (ボットのデータや関数定義 - この部分は変更なし)
+# ボットのデータや関数定義
 SHOT_TYPE = (
     (4, "紅霊夢A", "紅霊夢B", "紅魔理沙A", "紅魔理沙B"),
-    # ... (中略) ...
+    (6, "妖霊夢A", "妖霊夢B", "妖魔理沙A", "妖魔理沙B", "妖咲夜A", "妖咲夜B"),
+    (12, "永結界", "永幽冥", "永詠唱", "永紅魔", "永霊夢", "永紫", "永妖夢", "永幽々子", "永魔理沙", "永アリス", "永咲夜", "永レミリア"),
+    (6, "風霊夢A", "風霊夢B", "風霊夢C", "風魔理沙A", "風魔理沙B", "風魔理沙C"),
+    (6, "地霊夢A", "地霊夢B", "地霊夢C", "地魔理沙A", "地魔理沙B", "地魔理沙C"),
+    (6, "星霊夢A", "星霊夢B", "星魔理沙A", "星魔理沙B", "星早苗A", "星早苗B"),
+    (4, "神霊夢", "神魔理沙", "神早苗", "神妖夢"),
+    (6, "輝霊夢A", "輝霊夢B", "輝魔理沙A", "輝魔理沙B", "輝咲夜A", "輝咲夜B"),
+    (4, "紺霊夢", "紺魔理沙", "紺早苗", "紺鈴仙"),
+    (16, "春春", "春夏", "春秋", "春冬", "夏春", "夏夏", "夏秋", "夏冬", "秋春", "秋夏", "秋秋", "秋冬", "冬春", "冬夏", "冬秋", "冬冬"),
+    (9, "霊夢W", "霊夢E", "霊夢O", "魔理沙W", "魔理沙E", "魔理沙O", "妖夢W", "妖夢E", "妖夢O"),
     (4, "虹霊夢", "虹魔理沙", "虹咲夜", "虹早苗"),
 )
 STICKER = (
-    "<:kazusa:1318960518215766117>",
-    # ... (中略) ...
+    "<:kazusa:1318960518215766117>", "<:plana1:1318960569822351370>", "<:plana:1318960622268059728>",
+    "<:nyny:1318960704249663498>", "<:plana2:1318964188537815150>", "<:usio:1318964272038019132>",
     "<:chiaki:1318964308628996106>",
 )
 def get_random_shot():
     game = random.choice(SHOT_TYPE)
     return random.choice(game[1:])
 
-# (ボットのイベントハンドラ - この部分は変更なし)
+# ボットのイベントハンドラ
 @client.event
 async def on_ready():
     print(f'Bot準備完了～ Logged in as {client.user}')
@@ -48,8 +57,25 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user or message.author.bot:
         return
-    # ... (中略) ...
-    if "スタンプ" in message.content or "すたんぷ" in message.content:
+    if (client.user.mentioned_in(message) or any(keyword in message.content for keyword in ["本日の機体", "今日の機体", "きょうのきたい", "ほんじつのきたい", "イッツルナティックターイム！"])):
+        await message.channel.send(get_random_shot())
+        return
+    if "x.com" in message.content:
+        await message.channel.send(message.content.replace("x.com", "vxtwitter.com"))
+        return
+    if "www.pixiv.net" in message.content:
+        await message.channel.send(message.content.replace("www.pixiv.net", "www.phixiv.net"))
+        return
+    if any(keyword in message.content for keyword in ["にゃ～ん", "にゃーん"]):
+        await message.channel.send("にゃ～ん")
+        return
+    if any(keyword in message.content for keyword in ["説明!", "せつめい!"]):
+        await message.channel.send("今日の機体、本日の機体 またはメンションで機体出します")
+        return
+    if any(keyword in message.content for keyword in ["ソースコード", "そーす"]):
+        await message.channel.send("https://glitch.com/edit/#!/play-tohou?path=server.js%3A149%3A28")
+        return
+    if any(keyword in message.content for keyword in ["スタンプ", "すたんぷ"]):
         await message.channel.send(random.choice(STICKER))
         return
     if any(s in message.content for s in STICKER) or "💤" in message.content:
@@ -57,27 +83,18 @@ async def on_message(message):
         return
 
 # -----------------------------------------------------------------------------
-# ★★★ここからが重要な変更部分★★★
+# FlaskアプリとDiscordボットを同時に起動させるための設定
 # -----------------------------------------------------------------------------
-
 def run_bot():
-    """Discordボットを実行するための関数"""
     bot_token = os.environ.get("DISCORD_BOT_TOKEN")
     if not bot_token:
         print("DISCORD_BOT_TOKENが設定されていません。")
         return
-    
-    # イベントループを新しく作成して、その上でボットを実行する
-    # これにより、Gunicornのイベントループと衝突するのを防ぐ
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
     loop.run_until_complete(client.start(bot_token))
 
-# アプリケーションが起動したときに、一度だけボットをバックグラウンドで起動する
-# Gunicornがappをインポートした時点で、このスレッドが開始される
+# Gunicornがappをインポートした時点で、ボットをバックグラウンドで起動
 bot_thread = threading.Thread(target=run_bot)
 bot_thread.daemon = True
 bot_thread.start()
-
-# Gunicornは`app`という名前のFlaskインスタンスを探して実行するため、
-# `if __name__ == "__main__":`ブロックは不要になる。
